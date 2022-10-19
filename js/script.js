e16c64dc61b2825d464643b0c7f07e5fe15dd4bc
Alejandro Bryan <alejandrobryan023@gmail.com>
2022-10-19 20:00:02 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+alert("Hello World!")
+
+let app = document.querySelector('#app');
+ let  favoriteFood = "Dönner Kebap";
+ const firstName = 'Alejandro';
+ const lastName = 'Bryan';
+ const age = 32;
+ 
+ app.innerHTML =  `<p class="text-center">  My Name ${firstName} ${lastName}, I'm ${age} year old. </p>`;
+
+
+ document.write(' <p class="text-center">My favorite food is ',  favoriteFood, ' &#128523; </p>');
