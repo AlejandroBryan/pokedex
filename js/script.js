3eb3a3e897748fd15832124ecbfa53d6a770fce0
Alejandro Bryan <alejandrobryan023@gmail.com>
2022-10-28 20:08:11 +0200

    displaying our data into an ui

--- js/script.js
+++ js/script.js
@@ -1,30 +1,72 @@
+const app = document.querySelector('#app');
+let markUp = ' ';
+
 let pokemonList = [
     {
         name: "Bulbasaur",
+        index: 1,
         height: .7,
         weight: 6.9,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
         types: ['grass, poison'],
-        abilities:['Chlorophyll', 'Overgrow']
+        abilities:['chlorophyll', 'overgrow']
         
 
     },
     {
         name: 'Charmander',
+        index: 4,
         height: .6,
         weight: 8.5,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
         types: ['fire'],
         abilities: ['blaze', 'solar-power']
     },
     {
-        name: 'Onix',
-        height: 8.8,
-        weight: 210,
-        types: ['rock, ground'],
-        abilities: ['sturdy', 'rock-Headers', 'weak-armor']
+        name: 'Butterfree',
+        index: 12,
+        height: 1.1,
+        weight: 32,
+        image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png',
+        types: ['bug', 'flying'],
+        abilities: ['compoundeyes', 'tinted-EVs: lens']
     }
+
 
 ];
 
-console.log(pokemonList[2].name);
-console.log(pokemonList[1].name);
-console.log(pokemonList[0].name);
+
+
+for(let i = 0; i < pokemonList.length; i++){
+    print(pokemonList[i])
+
+    
+    markUp += `
+            <div class="card">
+                <span class="avatar" style="background-image: url(${pokemonList[i].image}); background-repeat: no-repeat;background-size: cover; background-position: center center"></span>
+
+                <span class="index">#${pokemonList[i].index}</span>
+                ${renderElements('types', pokemonList[i].types)}
+                <div class="card-body">
+                    <h2>${pokemonList[i].name} Height: ${pokemonList[i].height >= 1 ? pokemonList[i].height + ' - Wow that\'s big!' : pokemonList[i].height + ' '}</h2>
+                    <div>Weight: ${pokemonList[i].weight}</div>
+                    <h4>Abilities</h4>
+                    <div class="label-list">
+                    ${renderElements('label', pokemonList[i].abilities)}
+                    </div>
+                </div>
+
+            </div> 
+`;
+app.innerHTML = markUp;
+}
+
+
+function renderElements(atribute, elements) {
+    return ` ${elements.map(element => `<div class="${atribute}">  ${element} </div>`).join('')} `;
+}
+
+
+function print( arg){
+ console.log(arg);
+ }
