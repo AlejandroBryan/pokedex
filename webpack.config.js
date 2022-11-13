5c3b63cea9a376c5f498bb51c7808485830228bf
Alejandro Bryan <alejandrobryan023@gmail.com>
2022-11-13 20:44:57 +0100

    latest features

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+
+const path = require('path')
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'script.js',
+    path: path.resolve(__dirname, './dist/js/')
+  },
+  
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
